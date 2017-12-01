83663ca38d0c5d76369ebf0e48659beb74c4537e
Martin Bernstorff <ryqiem@gmail.com>
2017-12-01 17:48:48 +0100

    Updates

--- apps/singlebutton.py
+++ apps/singlebutton.py
@@ -15,6 +15,7 @@ class SingleButton(appapi.AppDaemon):
         self.log("Initializing {}".format(__name__))
 
         self.listen_event(self.button_single, "click", entity_id = "binary_sensor.switch_158d0001a1f52f", click_type = "single")
+        self.listen_event(self.long_click_press, "click", entity_id = "binary_sensor.switch_158d0001a1f52f", click_type = "long_click_press")
 
     def button_single(self, entity, attribute, old, new="", kwargs=""):
         if self.get_state("input_select.context") == "Sleep":
@@ -43,3 +44,10 @@ class SingleButton(appapi.AppDaemon):
                 self.set_state("input_select.playing_state", state = "playing")
 
             self.call_service("media_player/media_play_pause", entity_id = "media_player.rasplex")
+
+    def long_click_press(self, entity, attribute, old, new="", kwargs=""):
+        self.log("{} turned {}".format(entity, new))
+        if self.get_state("input_select.context") != "Pre-sleep":
+            self.set_state("input_select.context", state="Pre-sleep")
+        else:
+            self.set_state("input_select.context", state="Normal")
