4796d474853c43810530453f761adb7c7e99843c
Martin Bernstorff <ryqiem@gmail.com>
2018-08-14 21:09:28 +0200

    What: Added time-sensitivity for double_click Why: To avoid input_boolean.carpediem launching when going from presleep   to sleep

--- apps/switches/singlebutton_b.py
+++ apps/switches/singlebutton_b.py
@@ -46,54 +46,26 @@ class SingleButton_B(hass.Hass):
 
     def double_click(self, entity, attribute, old, new="", kwargs=""):
         # Define context-specific actions
+        # Double-click is moving forward in circadian contexts
         self.log("Double-click!")
-        if self.get_state("input_select.context") != "Cozy":
-            self.set_state("input_select.context", state = "Cozy")
-        else:
-            self.set_state("input_select.context", state = "Normal")
-
-    def long_click_press(self, entity, attribute, old, new="", kwargs=""):
         self.log("{} turned {}".format(entity, new))
         if self.get_state("input_select.context") == "Normal":
             self.set_state("input_select.context", state="Pre-sleep")
         if self.get_state("input_select.context") == "Pre-sleep":
+            self.set_state("input_select.context", state="Sleep")
+        if self.get_state("input_select.context") == "Sleep" and self.now_is_between("05:00:00", "18:00:00"):
+            self.turn_on("input_boolean.carpediem")
+
+    def long_click_press(self, entity, attribute, old, new="", kwargs=""):
+        self.log("Long_click_press!")
+        self.log("{} turned {}".format(entity, new))
+        if self.get_state("input_select.context") == "Normal":
+            self.set_state("input_select.context", state="Sleep")
+        if self.get_state("input_select.context") == "Pre-sleep":
             self.set_state("input_select.context", state="Normal")
         if self.get_state("input_select.context") == "Sleep":
-            if self.get_state("input_boolean.sunrise") == "off":
-                self.log("Manually starting sunrise")
-                self.turn_on("light.monitor", brightness = 255, transition = 600, xy_color = [0.32, 0.33])
-            if self.get_state("input_boolean.sunrise") == "on":
-                self.turn_off("input_boolean.sunrise")
-                self.turn_off("group.all_lights")
-                time.sleep(2)
-                self.turn_off("group.all_lights")
-
-    def condseq_on(self, switch=None, entity=None, brightness=None, t_fade=0, color=None, post_delay=0):
-        """
-            A function for conditional sequentilization
-            Takes the following arguments:
-
-            switch: An input boolean that's conditional for the sequence to
-            keep running
-
-            entity: The entity to be affected
-
-            For lights:
-            brightness: End brightness [0-255]
-            fade: How long the fade should take (in seconds)
-            color: End colour [X, Y]
-            post_delay: How long after the action there should be an additional delay (in seconds)
-        """
-        if switch is not None:
-            if self.get_state(switch) == "on":
-                if color is not None:
-                    self.turn_on(entity, brightness = brightness, transition = t_fade * self.modifier, xy_color = color)
-                    if self.get_state(switch) == "on":
-                        time.sleep(t_fade * self.modifier)
-                        time.sleep(post_delay * self.modifier)
-                else:
-                    self.turn_on(entity, brightness = brightness, transition = t_fade * self.modifier)
-                    if self.get_state(switch) == "on":
-                        time.sleep(t_fade * self.modifier)
-                        time.sleep(post_delay * self.modifier)
-            self.turn_on(entity)
+            if self.get_state("input_boolean.carpediem") == "off":
+                self.set_state("input_select.context", state="Pre-sleep")
+            elif self.get_state("input_boolean.carpediem") == "on":
+                self.turn_off("input_boolean.carpediem")
+                self.turn_on("input_boolean.good_night")
