4b6325d6aae618c9d2dc933adb4d6ef1fe0288a6
Martin Bernstorff <ryqiem@gmail.com>
2018-09-11 12:23:16 +0200

    Closes #1

--- apps/contexts/asleep.py
+++ apps/contexts/asleep.py
@@ -21,20 +21,7 @@ class Asleep(hass.Hass):
 
     def on_if_presleep(self, entity, attribute, old, new, kwargs):
         if self.get_state("input_select.context") == "Pre-sleep":
-            self.log("Starting good-night script")
-            self.turn_off("group.all_lights")
-            self.turn_off("media_player.pioneer")
-            self.turn_off("input_boolean.circadian")
-            self.turn_off("input_boolean.sunrise")
-            self.turn_off("input_boolean.carpediem")
-            self.turn_off("group.all_lights")
-            time.sleep(2)
-            self.turn_off("group.all_lights")
-            self.turn_off("media_player.pioneer")
-            self.turn_off("input_boolean.circadian")
-            self.turn_off("input_boolean.sunrise")
-            self.turn_off("input_boolean.carpediem")
-            self.turn_off("group.all_lights")
+            self.set_state("input_select.context", state = "Asleep")
 
     def on(self, entity, attribute, old, new, kwargs):
         self.log("Starting good-night script")
@@ -51,3 +38,4 @@ class Asleep(hass.Hass):
         self.turn_off("input_boolean.sunrise")
         self.turn_off("input_boolean.carpediem")
         self.turn_off("group.all_lights")
+        self.log("Finished good-night script, good night!")
