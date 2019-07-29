af14f66be46c7bda4d860322399b1cbce7839394
Martin <ryqiem@gmail.com>
2019-07-29 08:48:03 +0200

    Make alarm auto-set on first run, log info based on schedule handle

--- apps/circadian/sunrise.py
+++ apps/circadian/sunrise.py
@@ -22,13 +22,15 @@ class Sunrise(hass.Hass):
         self.log("Initializing sunrise")
         self.switch = self.args["switch"] # The switch to turn on/off the sunrise
 
-        self.entity = "light.bathroom_2" # The light to act as sun
+        self.light = "light.monitor" # The light to act as sun
 
-        self.listen_state(self.rise_instant, self.args["switch"], new="on") # Callback for testing
+        self.listen_state(self.rise_default, self.args["switch"], new="on") # Callback for testing
 
-        self.listen_state(self.set_alarm, "input_select.sunrise_hour") # Callback for testing
-        self.listen_state(self.set_alarm, "input_select.sunrise_minute") # Callback for testing
-        self.listen_state(self.set_alarm, "input_boolean.sunrise_alarm") # Callback for testing
+        self.set_alarm()
+
+        self.listen_state(self.set_alarm, "input_select.sunrise_hour")
+        self.listen_state(self.set_alarm, "input_select.sunrise_minute")
+        self.listen_state(self.set_alarm, "input_boolean.sunrise_alarm")
 
     def set_alarm(self, entity="", attribute="", old="", new="", kwargs=""):
         self.alarm = None
@@ -37,46 +39,35 @@ class Sunrise(hass.Hass):
             self.minute = self.get_state("input_select.sunrise_minute")# minute her
             self.alarm_time = self.parse_time("{}:{}:00".format(self.hour, self.minute))
 
-            self.log("Setting alarm to {}:{}".format(self.hour, self.minute))
-            self.alarm = self.run_daily(self.rise_instant, self.alarm_time)
+            self.alarm = self.run_daily(self.rise_default, self.alarm_time)
+            self.log("Set alarm to {}".format(self.info_timer(self.alarm)[0]))
 
-    def rise_instant(self, entity="", attribute="", old="", new="", kwargs=""):
+    def rise_default(self, entity="", attribute="", old="", new="", kwargs=""):
         self.modifier = 1
-        self.log("Rise_instant is running with switch {switch}, light {light} and modifier {modifier}".format(switch=self.switch, light=self.entity, modifier = self.modifier))
-        # self.condseq_on(switch=self.switch, entity=self.entity, brightness=1, t_fade=1, color=conv.rgb_to_xy(255, 0, 0))
-        self.condseq_on(switch=self.switch, entity="light.monitor", brightness=1, t_fade=10, color=3000)
-        self.condseq_on(switch=self.switch, entity="light.monitor", brightness=15, t_fade=20, color=3000)
-        self.condseq_on(switch=self.switch, entity="light.monitor", brightness=g.c_brightness, t_fade=570, color=3000)
-        self.condseq_on(switch=self.switch, entity="light.reol_2", brightness=g.c_brightness, t_fade=15, color=3000)
-        self.condseq_on(switch=self.switch, entity="light.hovedrum", brightness=g.c_brightness, t_fade=15, color=3000)
-        self.turn_on("input_boolean.circadian")
-
-    def rise_delay(self, entity="", attribute="", old="", new="", kwargs=""):
-        self.modifier = 1
-        self.delay = 75 # Time in minutes to wait for sunrise, 45 + smart period (30) for Sleep as Android default
-        self.turn_off("input_boolean.circadian")
-        self.log("The sun is rising in {} mins!".format(self.modifier * self.delay))
-        self.sunrise_timer = self.run_in(self.natural, self.modifier * self.delay*60)
+        self.log("Rise_default is running with switch {switch}, light {light} and modifier {modifier}".format(switch=self.switch, light=self.light, modifier = self.modifier))
+        # self.condseq_on(switch=self.switch, entity=self.light, brightness=1, t_fade=1, color=conv.rgb_to_xy(255, 0, 0))
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=1, t_fade=10, color=3000)
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=5, t_fade=1800, color=5000)
 
     #######################
     # Different sequences #
     #######################
 
     def red_only(self):
-        self.condseq_on(switch=self.switch, entity=self.entity, brightness=1, t_fade=1, color=[0.674, 0.322])
-        self.condseq_on(switch=self.switch, entity=self.entity, brightness=10, t_fade=1800, color=[0.674, 0.322])
-        self.condseq_on(switch=self.switch, entity=self.entity, brightness=100, t_fade=1800, color=[0.5268, 0.4133])
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=1, t_fade=1, color=[0.674, 0.322])
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=10, t_fade=1800, color=[0.674, 0.322])
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=100, t_fade=1800, color=[0.5268, 0.4133])
         self.turn_on("input_boolean.circadian")
 
     def natural(self, *args, **kwargs):
-        self.log("Natural sunrise is running with switch {switch}, light {light} and modifier {modifier}".format(switch=self.switch, light=self.entity, modifier = self.modifier))
-        # self.condseq_on(switch=self.switch, entity=self.entity, brightness=1, t_fade=1, color=conv.rgb_to_xy(255, 0, 0))
+        self.log("Natural sunrise is running with switch {switch}, light {light} and modifier {modifier}".format(switch=self.switch, light=self.light, modifier = self.modifier))
+        # self.condseq_on(switch=self.switch, entity=self.light, brightness=1, t_fade=1, color=conv.rgb_to_xy(255, 0, 0))
         self.condseq_on(switch=self.switch, entity="light.monitor", brightness=1, t_fade=1, color=3000)
         self.condseq_on(switch=self.switch, entity="light.monitor", brightness=1, t_fade=500, color=3000)
         self.condseq_on(switch=self.switch, entity="light.monitor", brightness=15, t_fade=400, color=3000)
-        self.condseq_on(switch=self.switch, entity=self.entity, brightness=1, t_fade=1, color=3000)
-        self.condseq_on(switch=self.switch, entity=self.entity, brightness=1, t_fade=899, color=3000)
-        self.condseq_on(switch=self.switch, entity=self.entity, brightness=255, t_fade=900, color=3000)
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=1, t_fade=1, color=3000)
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=1, t_fade=899, color=3000)
+        self.condseq_on(switch=self.switch, entity=self.light, brightness=255, t_fade=900, color=3000)
         self.turn_on("input_boolean.circadian")
 
     def condseq_on(self, switch=None, entity=None, brightness=None, t_fade=0, color=None, post_delay=0):
@@ -95,7 +86,7 @@ class Sunrise(hass.Hass):
             color: End colour [X, Y]
             post_delay: How long after the action there should be an additional delay (in seconds)
         """
-        device, entity_id = self.split_entity(self.entity)
+        device, entity_id = self.split_entity(self.light)
         if switch is not None:
             if self.get_state(switch) == "on":
                 if device == "light":
